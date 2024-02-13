89b201649474961b4f93846b5d55134a7796496d
hamza <hamzabenmoussa166@gmail.com>
2024-02-13 21:35:15 +0100

    firts commit

---
+++ src/main/java/com/example/ataaspringbootangular/AtaaSpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.ataaspringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaaSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaaSpringBootAngularApplication.class, args);
+	}
+
+}
